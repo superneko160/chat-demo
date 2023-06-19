9c100457f10baec51f880f04238740f6d907216e
superneko <supernekocat31@gmail.com>
2023-06-19 16:36:43 +0900

    fix message UI

--- js/script.js
+++ js/script.js
@@ -4,16 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
     let messageInput = document.getElementById('message-input');
     let sendButton = document.getElementById('send-button');
 
-    // 送信ボタン押下時
+    /* 
+     * 送信ボタン押下時
+     */
     sendButton.addEventListener('click', () => {
         let message = messageInput.value;
         if (message.trim() !== '') {
-            sendMessage(message);
+            sendMessage(message);  // サーバにメッセージ送信
+            displayMessage('self', message);  // 自分の送信したメッセージを表示
             messageInput.value = '';  // メッセージ入力欄クリア
         }
     });
 
-    // サーバにメッセージ送信
+    /*
+     *サーバにメッセージ送信
+     * @message: メッセージテキスト
+     */
     const sendMessage = (message) => {
         let request = new XMLHttpRequest();
         request.open('POST', 'server.php');
@@ -22,7 +28,7 @@ document.addEventListener('DOMContentLoaded', () => {
         request.onreadystatechange = () => {
             if (request.readyState === 4 && request.status === 200) {
                 let response = JSON.parse(request.responseText);
-                displayMessage(response.message);
+                displayMessage('server', response.message);
             }
         };
         // メッセージ送信
@@ -30,24 +36,47 @@ document.addEventListener('DOMContentLoaded', () => {
         request.send(data);
     };
 
-    // 画面にメッセージを表示
-    const displayMessage = (message) => {
+    /*
+     * 画面にメッセージを表示
+    * @user: ユーザ情報（self、server）
+    * @message: メッセージテキスト
+    */
+    const displayMessage = (user, message) => {
+        // パラメータチェック
+        if (user !== 'self' && user !== 'server') {
+            throw new Error('Invalid user value');
+        }
+
         // アイコンとメッセージ要素を包括する要素作成
         let bundle = document.createElement("div");
         bundle.className = "bundle";
+
         // アイコン要素作成
         let icon = document.createElement('img');
-        icon.src = "data/mujirushi-icon.png";
+        if (user === 'self') {
+            icon.src = "data/user-icon.png";
+        }
+        else {
+            icon.src = "data/server-icon.png";
+        }
         icon.alt = "icon";
         icon.className = 'icon';
         // アイコン要素の追加
         bundle.appendChild(icon);
+
         // メッセージ要素作成
         let messageElement = document.createElement('div');
         messageElement.className = 'message';
         messageElement.textContent =  message;
+        if (user === 'self') {
+            messageElement.classList.add('self-message');
+        }
+        else {
+            messageElement.classList.add('server-message');
+        }
         // メッセージ要素の追加
         bundle.appendChild(messageElement);
+
         // アイコンとメッセージ要素を包括する要素の追加
         chatLog.appendChild(bundle);
     };
